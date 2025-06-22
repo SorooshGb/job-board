d8fb866c57c271e23925a66b63019177155b373b
SorooshGb <acmaster777@yahoo.com>
2025-06-22 22:42:19 +0330

    feat: create the sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,36 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger
+} from '@/components/ui/sidebar';
+import { AppSidebarClient } from './_AppSidebarClient';
+
 export default function HomePage() {
-  return <h1>Hi</h1>;
+  return (
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl text-nowrap">Job Listing</span>
+          </SidebarHeader>
+          <SidebarContent>TEST</SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>test button</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+        <main className="flex-1">test content</main>
+      </AppSidebarClient>
+    </SidebarProvider>
+  );
 }
